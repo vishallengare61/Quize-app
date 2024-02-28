0eb594268fe0a2ab7abcd3e9f21001ffbebbfdcb
vishal <vishallengare61@gmail.com>
2024-02-28 17:47:07 +0530

    Add the dynamic attempt number and to dropdown to check the previous reports data

--- src/app/exam/quize-report/quize-report.component.ts
+++ src/app/exam/quize-report/quize-report.component.ts
@@ -7,6 +7,7 @@ import jsPDF from 'jspdf';
 import { LoginService } from 'src/Services/login.service';
 import { StudentHistoryService } from 'src/Services/student-history.service';
 import { TryReportService } from 'src/Services/try-report.service';
+import * as ApexCharts from 'apexcharts';
 
 @Component({
   selector: 'app-quize-report',
@@ -16,24 +17,10 @@ import { TryReportService } from 'src/Services/try-report.service';
 export class QuizeReportComponent implements OnInit  {
 
   showGraph: boolean = true;
-  hasSubjective: boolean = false;
-  highcharts = Highcharts;
   excellent: any;
   average: any;
   poor: any;
-  skippedPerc: any;
-  incorrectPerc: any;
-  correctPerc: any;
-  chartOptions = {};
-  chartOptionsa = {};
-  chartOptionsb = {};
-  chartOptionsc = {};
-  UserScore:any;
-  skippedScore:any;
-  wrongScore:any;
-  percentValue:any;
   correctAnswer:any ;
-  totalMarks:any ;
   totalQuestion:any ;
   unattempts:any;
   wrongAnswer:any ;
@@ -44,168 +31,207 @@ export class QuizeReportComponent implements OnInit  {
   resultPrediction: any;
   quizePoolId: any;
   attemptNumber: boolean = false;
-  
+  UserResultData: any;
+  totalSkipped: any;
+  correctAnswers: any;
+  quizeAttemptNumber: any;
+  attempDropdown: any;
+  chart: any;
+  chart2: any;
+  chart3: any;
   constructor(private _router: Router, private _route : ActivatedRoute, private _loginService:LoginService, private _tryService: TryReportService, private _historyService: StudentHistoryService){
 
   }
   ngOnInit(): void {
     this.quizePoolId = this._route.snapshot.paramMap.get('quizeId');  
-    this.allResultData = localStorage.getItem('result');
-    if (this.allResultData!=null) {
-      const paresedData = JSON.parse(this.allResultData);
-      this.correctAnswer = paresedData.correctAnswers;
-      this.totalMarks = paresedData.totalMarks;
-      this.totalQuestion = paresedData.totalQuestions;
-      this.unattempts = paresedData.unattempts;
-      this.wrongAnswer = paresedData.wrongAnswer;
-      this.totalTimeSpentForExam = paresedData.timeConsumed;
-      this.quiz_pool_name = paresedData.quiz_pool_name;
-      this.quize_date = paresedData.created_date;
+    this.getresultData(this.quizePoolId);
+}
+  getresultData(quizeID: any){
+  return new Promise((resolve) => {
+    this._historyService.getQuizeResult(quizeID).subscribe((result: any) => {
+      this.UserResultData = result.data;
+      this.totalQuestion = this.UserResultData.totalQuestions
+    this.correctAnswer = this.UserResultData.correctAnswers
+    this.unattempts = this.UserResultData.unattempts
+    this.wrongAnswer = this.UserResultData.wrongAnswer
+    this.quiz_pool_name = this.UserResultData.quiz_pool_name;
+    this.quize_date = this.UserResultData.created_date;
+    this.quizeAttemptNumber = this.UserResultData.currentAttemptNo;    
+    this.attempDropdown = this.UserResultData.allPositions;
+    this.totalTimeSpentForExam = this.UserResultData.timeConsumed;
+      this.updateGraphData();
+      resolve(this.UserResultData);
+    });
+  });
+
+}
+
+data(quizeID: any){
+  this.quizePoolId = quizeID.target.value;
+  // console.log('data getting id', this.quizePoolId);
+  // this.getresultData(this.quizePoolId);
+  return new Promise((resolve) => {
+    this._historyService.getQuizeResult(this.quizePoolId).subscribe((result: any) => {
+      this.UserResultData = result.data;
+      this.totalQuestion = this.UserResultData.totalQuestions
+    this.correctAnswer = this.UserResultData.correctAnswers
+    this.unattempts = this.UserResultData.unattempts
+    this.wrongAnswer = this.UserResultData.wrongAnswer
+    this.quiz_pool_name = this.UserResultData.quiz_pool_name;
+    this.quize_date = this.UserResultData.created_date;
+    this.quizeAttemptNumber = this.UserResultData.currentAttemptNo;    
+    // this.attempDropdown = this.UserResultData.allPositions;
+    this.totalTimeSpentForExam = this.UserResultData.timeConsumed;
+      this.updateGraphData();
+      resolve(this.UserResultData);
+    });
+  });
+
+}
+
+
+updateGraphData() {
+  this.clearCharts();
+  this.appexGraph(this.correctAnswer, this.wrongAnswer);
+  this.appexGraph2(this.unattempts, this.totalQuestion);
+  this.appexGraph3(this.wrongAnswer, this.totalQuestion);
+}
+
+clearCharts() {
+  const chartElements = document.querySelectorAll('.chart');
+  const chartElements2 = document.querySelectorAll('.chart2');
+  const chartElements3 = document.querySelectorAll('.chart3');
+  chartElements.forEach((element) => {
+    const chart = ApexCharts.exec(element.id, 'destroy');
+    if (chart) {
+      chart.destroy();
     }
-    // Set correct values for the charts
-    this.UserScore = this.correctAnswer; 
-    this.skippedScore = this.unattempts; 
-    this.wrongScore = this.wrongAnswer; 
-    this.percentValue = (this.correctAnswer / this.totalQuestion) * 100;
-    this.skippedPerc = ( this.unattempts / this.totalQuestion ) * 100;
-    this.incorrectPerc = (this.wrongAnswer / this.totalQuestion) * 100;
-    this.initGraph();
-    // this.initGrapha();
-    this.initGraphb();
-    this.initGraphc();
-    this.resultPrediction = this.percentValue.toFixed( 2 );
+  });
+  chartElements2.forEach((element) => {
+    const chart = ApexCharts.exec(element.id, 'destroy');
+    if (chart) {
+      chart.destroy();
+    }
+  });
+  chartElements3.forEach((element) => {
+    const chart = ApexCharts.exec(element.id, 'destroy');
+    if (chart) {
+      chart.destroy();
+    }
+  });
 }
 
-initGraph() {
-  this.chartOptions = {
-    chart: {
-      type: 'pie',
-      backgroundColor: '#F5F4F4'
-    },
-    credits: {
-      enabled: false
-    },
-    tooltip: { enabled: false },
-    title: {
-      text: `${this.percentValue.toFixed(2)}%`, 
-      verticalAlign: 'middle',
-      floating: true
-    },
-    plotOptions: {
-      pie: {
-        allowPointSelect: true,
-        cursor: 'pointer',
-        dataLabels: {
-          enabled: false,
-        }
-      }
-    },
-    colors: [
-      this.UserScore > 0 ? '#1FD115' : '#808080',
-      this.UserScore < this.totalQuestion ? '#FF1900' : '#808080'
-    ],
-    series: [{
-      innerSize: '80%',
-      data: [{
-        name: `Correct`,
-        y: this.UserScore
+appexGraph(answered: any, wrong: any) {
+  const answeredCount = answered;
+  const skippedCount = wrong;
+
+  // Check if chart instance exists
+  if (this.chart) {
+    // Update series data dynamically
+    this.chart.updateSeries([answeredCount, skippedCount]);
+  } else {
+    const options = {
+      chart: {
+        height: 350,
+        width: 350,
+        type: 'donut', 
       },
-      {
-        name: `InCorrect`,
-        y: this.totalQuestion - this.UserScore
-      }]
-    }],
-  };
-
-}
-
-initGraphb() {
-  this.chartOptionsb = {   
-    chart: {
-      height: 150,
-      width: 150,
-      type: 'pie',
-      backgroundColor: '#FFFFFF'
-    },
-    credits: {
-      enabled: false
-    },
-    tooltip: { enabled: false },
-    title: {
-      text: `${this.skippedPerc.toFixed(2)}%`,
-      verticalAlign: 'middle',
-      floating: true
-    },
-    plotOptions: {
-      pie: {
-        allowPointSelect: true,
-        cursor: 'pointer',
-        dataLabels: {
-          enabled: false,
+      series: [answeredCount, skippedCount], 
+      labels: ['Correct', 'Incorrect'], 
+      dataLabels: {
+        enabled: false,
+        show: false,
+        formatter: function (val:any, opts:any) {
+          return opts.w.globals.labels[opts.seriesIndex] + ": " + val;
         }
-      }
-    },
-    colors: [
-      this.skippedScore > 0 ? '#9460ff' : '#808080', 
-      this.skippedScore < this.totalQuestion ? '#e7eaeb' : '#808080'
-    ],
-    series: [{
-      innerSize: '80%',
-      data: [{
-        name: `Correct`,
-        y: this.skippedScore
       },
-      {
-        name: `InCorrect`,
-        y: this.totalQuestion - this.skippedScore
-      }]
-    }],
-  };
-}
-
-initGraphc() {
-this.chartOptionsc = {   
-   chart: {
-    height: 150,
-    width: 150,
-      type: 'pie',
-      backgroundColor : '#FFFFFF'
-   },
-   credits: {
-    enabled: false
-  },
-  tooltip: { enabled: false },
-   title: {
-    text: `${this.incorrectPerc.toFixed(2)}%`,
-    verticalAlign: 'middle',
-    floating: true
-   },
-  plotOptions: {
-    pie: {
-      allowPointSelect: true,
-      cursor: 'pointer',
-          dataLabels: {
-              enabled: false,
+      plotOptions: {
+        pie: {
+          donut: {
+            size: '70%' 
           }
-    }
-  },
-  colors: [
-    this.wrongScore > 0 ? '#ffa500' : '#808080', 
-    this.wrongScore < this.totalQuestion ? '#e7eaeb' : '#808080'
-],
-series: [{
-  innerSize: '80%',
-  data: [{
-    name: `Correct`,
-    y: this.wrongScore
-  },
-  {
-    name: `InCorrect`,
-    y: this.totalQuestion - this.wrongScore
-  }]
-}],
+        }
+      },
+      colors: ['#1FD115', '#FF1900'] 
+    };
 
-};
+    this.chart = new ApexCharts(document.querySelector("#chart"), options);
+    this.chart.render();
+  }
+}
+appexGraph2(skipped: any, total: any) {
+  const skippedCount = skipped;
+  const answeredCount = total;
+
+  // Check if chart instance exists
+  if (this.chart2) {
+    // Update series data dynamically
+    this.chart2.updateSeries([skippedCount, answeredCount]);
+  } else {
+    const options = {
+      chart: {
+        height: 250,
+        width: 250,
+        type: 'donut', 
+      },
+      series: [skippedCount, answeredCount], 
+      labels: ['skipped', 'total'], 
+      dataLabels: {
+        enabled: false,
+        formatter: function (val:any, opts:any) {
+          return opts.w.globals.labels[opts.seriesIndex] + ": " + val;
+        }
+      },
+      plotOptions: {
+        pie: {
+          donut: {
+            size: '70%' 
+          }
+        }
+      },
+      colors: ['#ffa500', '#6d7fcc'] 
+    };
+
+    this.chart2 = new ApexCharts(document.querySelector("#chart2"), options);
+    this.chart2.render();
+  }
+}
+appexGraph3(incorrect: any, total: any) {
+  const answeredCount = incorrect;
+  const skippedCount = total;
+
+  // Check if chart instance exists
+  if (this.chart3) {
+    // Update series data dynamically
+    this.chart3.updateSeries([answeredCount, skippedCount]);
+  } else {
+    const options = {
+      chart: {
+        height: 250,
+        width: 250,
+        type: 'donut', 
+      },
+      series: [answeredCount, skippedCount], 
+      labels: ['incorrect', 'total'], 
+      dataLabels: {
+        enabled: false,
+        formatter: function (val:any, opts:any) {
+          return opts.w.globals.labels[opts.seriesIndex] + ": " + val;
+        }
+      },
+      plotOptions: {
+        pie: {
+          donut: {
+            size: '70%' 
+          }
+        }
+      },
+      colors: ['#FF1900', '#6d7fcc'] 
+    };
+
+    this.chart3 = new ApexCharts(document.querySelector("#chart3"), options);
+    this.chart3.render();
+  }
 }
 
 generatePDF() {
@@ -224,6 +250,5 @@ generatePDF() {
 });
 }, 800);
 }
-
 
 }
